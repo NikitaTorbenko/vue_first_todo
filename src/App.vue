4a2a2e7db2b5ddb19246de995a91cf481abb12f8
NikitaTorbenko <2015nekitciti@gmail.com>
2023-03-04 13:16:32 +0300

    done

---
+++ src/App.vue
@@ -0,0 +1,98 @@
+<template>
+  <main>
+    <TaskInput @onAddTask="addTask" />
+    <ul class="task-list my-list">
+      <li v-for="item in taskList" :key="item.id">
+        <TaskCard
+          @onDone="setDoneTask(item.id)"
+          @onRemove="removeTask(item.id)"
+          :model="item"
+        />
+      </li>
+    </ul>
+  </main>
+</template>
+
+<script>
+import TaskCard from "./components/TaskCard.vue";
+import TaskInput from "./components/TaskInput.vue";
+import { ref } from "vue";
+
+export default {
+  components: {
+    TaskCard,
+    TaskInput,
+  },
+  setup() {
+    const taskList = ref([
+      {
+        id: 0,
+        title: "Написать тудуху",
+        description: "нгрукащуоцв",
+        status: false,
+      },
+    ]);
+
+    const addTask = ({ title, description }) => {
+      taskList.value = [
+        ...taskList.value,
+        {
+          id: taskList.value[taskList.value.length - 1].id + 1,
+          title,
+          description,
+          status: false,
+        },
+      ];
+    };
+
+    const setDoneTask = (id) => {
+      taskList.value = taskList.value.map((x) => {
+        if (x.id === id) x.status = true;
+        return x;
+      });
+    };
+    const removeTask = (id) => {
+      taskList.value = taskList.value.filter((x) => x.id !== id);
+    };
+
+    return {
+      taskList,
+      addTask,
+      removeTask,
+      setDoneTask,
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+* {
+  font-family: serif;
+  margin: 0;
+  padding: 0;
+}
+
+main {
+  min-width: 450px;
+  margin: 0 auto;
+}
+
+button,
+input,
+.my-style {
+  border-radius: 5px;
+  border: 1px solid lightgray;
+  padding: 10px;
+}
+
+.my-list {
+  display: grid;
+  grid-template-rows: auto;
+  grid-template-columns: 100%;
+  grid-gap: 5px;
+}
+
+.task-list {
+  list-style: none;
+}
+</style>
---
+++ src/components/TaskCard.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="task-card my-style">
+    <div>
+      <h4>{{ model.title }}</h4>
+      <p>{{ model.description }}</p>
+    </div>
+    <div>
+      <button @click="emitOnDone" v-if="!model.status">Выполнить задачу</button>
+      <button @click="emitOnRemove" v-else>Удаление задачи</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  emits: ["onDone", "onRemove"],
+  props: {
+    model: {
+      required: true,
+      default: {
+        id: 0,
+        title: "Написать тудуху",
+        description: "Еще можно будет сделать на ts",
+        status: false,
+      },
+    },
+  },
+  setup(props, { emit }) {
+    const emitOnDone = () => emit("onDone");
+    const emitOnRemove = () => emit("onRemove");
+
+    return {
+      emitOnDone,
+      emitOnRemove,
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+.task-card {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
